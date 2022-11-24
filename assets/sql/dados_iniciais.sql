aecb43821c3371247387a542f36acf8d311b5302
Rafael8000 <rafael.pomari@etec.sp.gov.br>
2022-11-24 14:03:42 +0000

    Preparação da base de dados

---
+++ assets/sql/dados_iniciais.sql
@@ -0,0 +1,11 @@
+INSERT INTO  generos (id, nome) VALUES
+(1, 'Alta fantasia'),
+(2, 'Policial'),
+(3, 'Drama'),
+(4, 'Ficção Científica');
+
+INSERT INTO  livros (id, titulo, id_genero) VALUES
+(1, 'Assassino no Expresso do Oriente', 2),
+(2, 'A sociedade do anel', 1),
+(3, 'como eu era antes de você', 3),
+(4, 'O guia do mochileiro das galáxias', 4);
